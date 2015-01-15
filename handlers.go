e75fe3ac2dbf33a4b01ed3deb8bc2f18c36e9f26
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-01-15 15:58:40 +0000

    Store JSON in Mongo
    Persist the data that is POSTed in a Mongo collection. Don't do
    any kind of validation or sanitisation yet.

--- handlers.go
+++ handlers.go
@@ -4,22 +4,58 @@ import (
 	"encoding/json"
 	"io/ioutil"
 	"net/http"
+	"time"
+
+	"labix.org/v2/mgo"
+)
+
+var (
+	mgoSession      *mgo.Session
+	mgoDatabaseName = getenvDefault("JSON_REPORT_CATCHER_MONGO_DB", "json_report_catcher")
+	mgoURL          = getenvDefault("JSON_REPORT_CATCHER_MONGO_URL", "localhost")
 )
 
 type CspReport struct {
-	Details CspDetails `json:"csp-report"`
+	Details    CspDetails `json:"csp-report" bson:"csp_report"`
+	ReportTime time.Time  `bson:"date_time"`
 }
 
 type CspDetails struct {
-	DocumentUri       string `json:"document-uri"`
-	Referrer          string `json:"referrer"`
-	BlockedUri        string `json:"blocked-uri"`
-	ViolatedDirective string `json:"violated-directive"`
-	OriginalPolicy    string `json:"original-policy"`
+	DocumentUri       string `json:"document-uri" bson:"document_uri"`
+	Referrer          string `json:"referrer" bson:"referrer"`
+	BlockedUri        string `json:"blocked-uri" bson:"blocked_uri"`
+	ViolatedDirective string `json:"violated-directive" bson:"violated_directive"`
+	OriginalPolicy    string `json:"original-policy" bson:"original_policy"`
+}
+
+func getMgoSession() *mgo.Session {
+	if mgoSession == nil {
+		var err error
+		mgoSession, err = mgo.Dial(mgoURL)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return mgoSession.Clone()
+}
+
+func storeCspReport(report CspReport) {
+	session := getMgoSession()
+	defer session.Close()
+	session.SetMode(mgo.Strong, true)
+
+	collection := session.DB(mgoDatabaseName).C("reports")
+
+	err := collection.Insert(report)
+
+	if err != nil {
+		panic(err)
+	}
+
 }
 
 // JsonReceiverHandler receives JSON from a request body
-// TODO: Validate and store the JSON
+// TODO: Validate the JSON
 func JsonReceiverHandler(w http.ResponseWriter, req *http.Request) {
 	var newCspReport CspReport
 
@@ -36,6 +72,10 @@ func JsonReceiverHandler(w http.ResponseWriter, req *http.Request) {
 		http.Error(w, "Error parsing JSON", http.StatusBadRequest)
 		return
 	}
+
+	newCspReport.ReportTime = time.Now().UTC()
+
+	go storeCspReport(newCspReport)
 
 	w.Write([]byte("JSON received"))
 }
