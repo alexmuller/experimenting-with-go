a5f4fc9bb9e4cdcc0ce277456df58941df61999a
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2015-01-16 17:04:10 +0000

    Don't use a WaitGroup
    Matt B pointed out that there's no need for a WaitGroup here.

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"sync"
 
 	"github.com/alext/tablecloth"
 )
@@ -22,14 +21,6 @@ func getenvDefault(key string, defaultVal string) string {
 	return val
 }
 
-func catchListenAndServe(addr string, handler http.Handler, ident string, wg *sync.WaitGroup) {
-	defer wg.Done()
-	err := tablecloth.ListenAndServe(addr, handler, ident)
-	if err != nil {
-		log.Fatal(err)
-	}
-}
-
 func main() {
 	if wd := os.Getenv("GOVUK_APP_ROOT"); wd != "" {
 		tablecloth.WorkingDir = wd
@@ -38,11 +29,6 @@ func main() {
 	publicMux := http.NewServeMux()
 	publicMux.HandleFunc("/r", JsonReceiverHandler)
 
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-
-	go catchListenAndServe(reportingPort, publicMux, "reports", wg)
 	log.Println("json-report-catcher: listening for reports on " + reportingPort)
-
-	wg.Wait()
+	log.Fatal(tablecloth.ListenAndServe(reportingPort, publicMux, "reports"))
 }
